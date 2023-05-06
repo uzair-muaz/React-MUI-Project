bfab1f4f38a5df3e5eff4641f82d6208e1d53bf6
Uzair <uzair.muaz3k@gmail.com>
2023-05-07 01:49:16 +0500

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Box, CssBaseline, Stack } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+
 function App() {
   return (
-    <div >
-      hello
-    </div>
+    <>
+      <CssBaseline />
+      <Box>
+        <Navbar />
+        <Stack direction='row' justifyContent='space-between' spacing={2}>
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+      </Box>
+    </>
   );
 }
 
